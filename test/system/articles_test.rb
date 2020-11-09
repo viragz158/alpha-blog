e72c49b96b3543c8e408f486122faf84bbe2948f
Virag Zaveri <viragz18@gmail.com>
2020-11-08 21:55:21 -0500

    Progress on articles site and added system tests

---
+++ test/system/articles_test.rb
@@ -0,0 +1,27 @@
+require "application_system_test_case"
+
+class ArticlesTest < ApplicationSystemTestCase
+  test "visiting the new articles page" do
+    visit new_article_url
+  
+    assert_selector "h1", text: "Create a new Article"
+  end
+
+  test "viewing the index" do
+    visit articles_path
+    assert_selector "h1", text: "Articles listing page"
+  end
+
+  test "creating an article" do
+    visit new_article_url
+   
+    fill_in "Title", with: "Creating an Article"
+    fill_in "Description", with: "Created this article successfully!"
+   
+    click_on "Save Article"
+   
+    assert_text "Article was created successfully."
+    assert_text "Creating an Article"
+  end
+
+end
