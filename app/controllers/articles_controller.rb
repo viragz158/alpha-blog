68ea902b4b62d4ad56c59de194cc55064fabdcf3
Virag Zaveri <viragz18@gmail.com>
2020-12-20 13:24:18 -0500

    Remove repeated params code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     
     def create
         # render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created successfully."
             redirect_to article_path(@article)
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
@@ -46,6 +46,10 @@ class ArticlesController < ApplicationController
 
     def set_article
         @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
