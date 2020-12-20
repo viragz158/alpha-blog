a2988fde1c9818b1fc3cd4acaba4caccad510570
Virag Zaveri <viragz18@gmail.com>
2020-12-20 13:21:55 -0500

    Remove the repeated code to find the article by ID

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     
     def show
-        @article = Article.find(params[:id])
     end
     
     def index
@@ -13,7 +14,6 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     
@@ -29,7 +29,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(params.require(:article).permit(:title, :description))
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
@@ -39,10 +38,15 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
-    
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
 end
 
