47b5ca3062b20d1f66a0fb5a704b102fd3edd8e9
Virag Zaveri <viragz18@gmail.com>
2021-01-12 00:41:58 -0500

    Adding integration with BootStrap with Navbar demo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,9 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
